e599fda12d845b9a7cb5f276da5bc30f12802c83
rinaido022 <rinaido022@student.wethinkcode.co.za>
2023-05-03 16:03:35 +0200

    Researching YouTube API
    Adding Documentation of relevant information retrieved from reading API Documentation.
    
    We have two methods to query videos:
    - Standard HTTP Request, then parse the document and search for key fields such as 'playability'
    - via the YouTube API (See Videos:list) we can query several video IDs and determine which are actually playable.

--- main.py
+++ main.py
@@ -14,31 +14,3 @@ with open("response.html", 'w') as f:
     f.write(response.text)
 
 print("DONE.")
-
-"""
-In response.text :
-
-Can be Played:
-
-    "playabilityStatus":{
-        "status":"OK",
-        "playableInEmbed":true,
-        "miniplayer":{
-            "miniplayerRenderer":{
-                "playbackMode":"PLAYBACK_MODE_ALLOW"
-                }
-            }
-
-
-Can't be Played:
-
-    "playabilityStatus":{
-        "status":"ERROR",
-        "reason":"Video unavailable",
-        "errorScreen":{
-            "playerErrorMessageRenderer":{
-                "reason":{
-                    "simpleText":"Video unavailable"
-                    }
-                
-"""
