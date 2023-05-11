c3f903f2c26756c94129a3e27be987281bdd2f37
cuswan022 <cuswan022@student.wethinkcode.co.za>
2023-05-11 17:21:53 +0200

    Text input GUI takes a link and checks for valid URLs

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def compile_id_string(id_set):
     return id_string
 
 
-if __name__ == '__main__':
+def check_response_for_valid_ids(response):
 
-    ids_to_validate = {
-        "g_FTlm3sdq12tdoU",
-        "g_FTlm3tdoU"
-    }
+    valid_ids = set()
+    for item in response.get("items"):
+        valid_ids.add(item.get("id"))
+    return valid_ids
+
+
+def send_request(ids_to_validate):
 
     youtube_service = youtube.dev_auth("config/dev_key.txt")
 
@@ -23,12 +26,14 @@ if __name__ == '__main__':
         part="player",
         id=compile_id_string(ids_to_validate)
     )
-
     response = request.execute()
+    return response
 
-    valid_ids = set()
-    for item in response.get("items"):
-        valid_ids.add(item.get("id"))
 
+def final_validation(ids_to_validate):
+    response = send_request(ids_to_validate)
+    valid_ids = check_response_for_valid_ids(response)
     invalid_ids = set.difference(ids_to_validate, valid_ids)
     print(f"These are invalid ids : {invalid_ids}")
+
+
