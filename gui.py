1ffa117efa5766267b996ccc76cac19415ac767a
cuswan022 <cuswan022@student.wethinkcode.co.za>
2023-05-29 13:14:21 +0200

    Gui layout done needs to have events on the buttons

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.app import App
@@ -6,11 +6,15 @@ from kivy.app import App
 
 class MyApp(App):
 
-    layout = GridLayout(cols=1)
-
     def build(self):
-        btn = Button(text="hello")
-        self.layout.add_widget(btn)
+        layout = FloatLayout()
+        add_button = Button(text="Add", pos_hint={'right': .985, 'top': .5}, size_hint=(.1, .05))
+        url_get = TextInput(text="Youtube url", size_hint=(.8, .05), pos_hint={'right': .85, 'top': .5})
+        validate_button = Button(text="Validate", size_hint=(.4, .05), pos_hint={'right': .7, 'top': .43})
+        layout.add_widget(url_get)
+        layout.add_widget(add_button)
+        layout.add_widget(validate_button)
+        return layout
 
 
 if __name__ == "__main__":
