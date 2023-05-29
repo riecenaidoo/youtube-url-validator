22493f133b14c6b5c6dd62861b7c2fc0d72f047e
cuswan022 <cuswan022@student.wethinkcode.co.za>
2023-05-29 13:18:51 +0200

    Gui layout done needs to have events on the buttons

--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ class MyApp(App):
         add_button = Button(text="Add", pos_hint={'right': .985, 'top': .5}, size_hint=(.1, .05))
         url_get = TextInput(text="Youtube url", size_hint=(.8, .05), pos_hint={'right': .85, 'top': .5})
         validate_button = Button(text="Validate", size_hint=(.4, .05), pos_hint={'right': .7, 'top': .43})
+        add_button.on_release()
         layout.add_widget(url_get)
         layout.add_widget(add_button)
         layout.add_widget(validate_button)
