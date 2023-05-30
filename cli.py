51a14678c24f1223a62440d5b31eade4a7544453
SuperMagikarp <cu4nsw4n@gmail.com>
2023-05-30 08:22:57 +0200

    3 interface cli (#11)
    * First draft
    
    * CLI for youtube validator needs oauth key
    
    * Youtube validator CLI
    
    * deleted duplicate gui file
    
    * Removed Virtual Environment
    
    * Bugfixes
    
    * Replacing old regex with the simplified regex from the GUI branch
    * Fixed the User input being lowered, before it is added to the validation list (ID links are case-sensitive)
    
    ---------
    
    Co-authored-by: cuswan022 <cuswan022@student.wethinkcode.co.za>
    Co-authored-by: riecenaidoo <riecenaidoo@outlook.com>

---
+++ cli.py
@@ -0,0 +1,30 @@
+import re
+from main import final_validation
+
+ids_to_check = set()
+
+
+def validate_then_add_to_list(text):
+    pattern = re.compile("(v=[_0-9A-Za-z]{11})")
+    m = re.search(pattern, text)
+    if m:
+        ids_to_check.add(m.group(1)[2:])
+    print(ids_to_check)
+
+
+def run():
+    while True:
+        url = input("Enter your url: ")
+        global ids_to_check
+        if url.lower() == 'validate':
+            final_validation(ids_to_check)
+            ids_to_check = set()
+        elif url.lower() == 'quit':
+            print('Goodbye! ')
+            break
+        else:
+            validate_then_add_to_list(url)
+
+
+if __name__ == "__main__":
+    run()
